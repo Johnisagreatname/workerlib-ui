0b6eeac1cacdf6187ead1e509bd509d65aff1f40
stark <784184859@qq.com>
2019-10-29 10:12:21 +0800

    添加文件

---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import "@/assets/css/common.css";
+
+    import { Component, Vue, Prop, Model} from 'vue-property-decorator';
+
+    @Component({
+        components:{
+        },
+        directives: { // 自定义指令
+        },
+        mounted() {
+        },
+        computed: {
+            menuitemClasses: function () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        }
+    })
+    export default class Nav extends Vue {
+        @Model('isCollapsed', { type: Boolean }) private isCollapsed!: boolean;
+
+        clicks(e) : void {
+          this['$router'].push(e)
+        }
+    }
+</script>
+
+<style scoped src="@/styles/nav.css" />
+<template lang="pug" src="@/views/nav.pug" />
+
