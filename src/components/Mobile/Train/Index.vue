204f36897f8f7acaedcdbf3e790d5315566829db
cltq <541989785@qq.com>
2019-12-28 14:08:36 +0800

    h5

---
+++ src/components/Mobile/Train/Index.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import {Component, Vue, Prop, Model} from 'vue-property-decorator';
+    import {getModule} from 'vuex-module-decorators';
+    import {Tab, TabItem, Sticky,ViewBox} from 'vux'
+
+    @Component({
+        components: {
+            Sticky,
+            ViewBox,
+            Tab,
+            TabItem
+        }
+    })
+    export default class Train extends Vue {
+
+    }
+</script>
+<template lang="pug" src="@/views/mobile/train.pug"/>
+<style scoped src="@/styles/mobile/train.css"/>
---
+++ src/store/mobile/MobileStore.ts
@@ -0,0 +1,138 @@
+import {Module, VuexModule, Mutation, Action} from 'vuex-module-decorators';
+import store from "../index";
+import request from "../../common/HttpClient";
+import {Message} from "iview";
+import MessageUtils from "../../common/MessageUtils";
+import router from '../../router/.invoke/router'
+
+
+@Module({
+    namespaced: true,
+    stateFactory: true,
+    dynamic: true,
+    name: "MobileStore",
+    store,
+})
+export default class MobileStore extends VuexModule {
+    public conditionList: Array<any>;
+    public conditionListp: Array<any>;
+    public userId: string | (string | null)[];
+    public personInfo: any;
+    public projectInfo: Array<any>;
+    public attendances: any;
+    public wages: any;
+    public evaluates: any;
+
+    constructor(e) {
+        super(e);
+        this.conditionList = [];
+        this.conditionListp = [];
+        this.projectInfo = [];
+        this.personInfo = null;
+        this.attendances = null;
+        this.wages = null;
+        this.evaluates = null;
+    }
+
+    @Action
+    public getParams(): any {
+        let item = {};
+    debugger
+        this.userId = router.currentRoute.query.kk;
+        item["name"] = "eafId";
+        item["value"] = this.userId;
+        item["algorithm"] = "EQ";
+        this.conditionList.push(item);
+        return {
+            "pageInfo": {},
+
+            "conditionList": this.conditionList,
+
+            "sortList": [],
+
+            "groupList": [],
+
+            "keywords": [],
+
+            "selectList": []
+        };
+    }
+    @Action
+    public getParamsInfo(): any {
+        let item = {};
+    debugger
+        this.userId = router.currentRoute.query.kk;
+        item["name"] = "archives_id";
+        item["value"] = this.userId;
+        item["algorithm"] = "EQ";
+        this.conditionListp.push(item);
+        return {
+            "pageInfo": {},
+
+            "conditionList": this.conditionListp,
+
+            "sortList": [],
+
+            "groupList": [],
+
+            "keywords": [],
+
+            "selectList": []
+        };
+    }
+    @Action
+    public async selectPersonInfo() {
+    debugger;
+        await request.post('/api/workerlib/alluser', await this.getParams()).then((data) => {
+            this.success(data);
+        }).catch((e) => {
+        debugger;
+            let alert: any = Message;
+            if (!e) {
+                alert.warning('未知错误！')
+                return
+            }
+            if (e.response && e.response.data && e.response.data.message) {
+                alert.warning(e.response.data.message)
+                return
+            }
+            if (!e.message) {
+                return;
+            }
+            alert.warning(e.message || e)
+        });
+    }
+
+    @Mutation
+    private success(data: any) {
+    debugger;
+        this.personInfo = data.data[0];
+    }
+
+    @Action
+    public async selectAttendances() {
+        await request.post('/api/workerlib/archives', await this.getParamsInfo()).then((data) => {
+            this.successProjectList(data);
+        }).catch((e) => {
+            let alert: any = Message;
+            if (!e) {
+                alert.warning('未知错误！')
+                return
+            }
+            if (e.response && e.response.data && e.response.data.message) {
+                alert.warning(e.response.data.message)
+                return
+            }
+            if (!e.message) {
+                return;
+            }
+            alert.warning(e.message || e)
+        });
+    }
+
+    @Mutation
+    private successProjectList(data: any) {
+    debugger;
+        this.projectInfo = data.data;
+    }
+}
