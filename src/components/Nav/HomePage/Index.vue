106e4c5930061c8883b736c3966393e1bd629d46
stark <784184859@qq.com>
2020-04-29 10:01:27 +0800

    首页

--- src/components/Nav/HomePage/Index.vue
+++ src/components/Nav/HomePage/Index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date         : 2020-04-28 09:46:19
  * @LastEditors  : HaoJie
- * @LastEditTime : 2020-04-28 11:30:36
+ * @LastEditTime : 2020-04-28 18:27:16
  * @FilePath     : \src\components\Nav\HomePage\Index.vue
  -->
 <script lang="ts">
@@ -9,20 +9,76 @@ import "@/assets/css/common.css";
 import HomePageStore from "../../../store/modules/HomePageStore";
 import { Component, Vue, Prop, Model, Watch } from "vue-property-decorator";
 import { getModule } from "vuex-module-decorators";
+import "@/assets/css/common.css";
+import Echart from "echarts";
+Vue.prototype.$echarts = Echart;
 
-@Component({
-  data() {},
-  components: {},
-  directives: {
-    // 自定义指令
-  }
-})
+@Component({})
 export default class HomePage extends Vue {
   private store: any;
+  private jobsType: string;
   constructor() {
     super();
     this.store = getModule(HomePageStore);
+    this.jobsType = "own";
   }
+  mounted() {
+    this.getJobs();
+  }
+  async getJobs() {
+    let data: Array<jobs>;
+    switch (this.jobsType) {
+      case "own":
+        data = await this.store.getJobs("own");
+        this.jobs(data);
+        break;
+      case "epiboly":
+        data = await this.store.getJobs("epiboly");
+        this.jobs(data);
+        break;
+      default:
+        break;
+    }
+  }
+  jobs(data: Array<jobs> = [{ value: 0, name: "无数据", color: '#2f5ee4' }]) {
+    let color = data.map(a => a.color)
+    let list = data.map(a => {
+      return {
+        value: a.value,
+        name: a.name
+      }
+    })
+    const jobs = document.getElementById("jobs");
+    const chart: any = this["$echarts"].init(jobs);
+    chart.setOption({
+      color: color,
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+      },
+      series: [
+        {
+          name: "工种人数统计",
+          type: "pie",
+          radius: ["50%", "80%"],
+          // avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: true,
+              position: "outside",
+              color: "#747fa0"
+            }
+          },
+          data: list
+        }
+      ]
+    });
+  }
+}
+interface jobs {
+  value: number;
+  name: string;
+  color: string;
 }
 </script>
 
--- src/store/modules/HomePageStore.ts
+++ src/store/modules/HomePageStore.ts
@@ -1,7 +1,7 @@
 /*
  * @Date         : 2020-04-28 10:01:44
  * @LastEditors  : HaoJie
- * @LastEditTime : 2020-04-28 10:05:31
+ * @LastEditTime : 2020-04-28 18:28:38
  * @FilePath     : \src\store\modules\HomePageStore.ts
  */
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
@@ -21,6 +21,18 @@ export default class HomePageStore extends VuexModule {
   constructor(e) {
     super(e);
   }
-  // @Action
+  @Action
+  async getJobs(key) {
+    switch (key) {
+      case "own":
+        // 这里请求自有工人数据
+        return [{ name: "自有工人", value: 123, color: "pink" }];
+      case "epiboly":
+        // 这里请求分包工人数据
+        return [{ name: "分包工人", value: 123, color: "red" }];
+      default:
+        return
+    }
+  }
   // @Mutation
 }
