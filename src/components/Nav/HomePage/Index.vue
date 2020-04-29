2575cec163be5282ae340f0daa6a3af05c7f144c
stark <784184859@qq.com>
2020-04-29 20:15:26 +0800

    首页静态页面完成

--- src/components/Nav/HomePage/Index.vue
+++ src/components/Nav/HomePage/Index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date         : 2020-04-28 09:46:19
  * @LastEditors  : HaoJie
- * @LastEditTime : 2020-04-28 18:27:16
+ * @LastEditTime : 2020-04-29 19:28:30
  * @FilePath     : \src\components\Nav\HomePage\Index.vue
  -->
 <script lang="ts">
@@ -17,6 +17,87 @@ Vue.prototype.$echarts = Echart;
 export default class HomePage extends Vue {
   private store: any;
   private jobsType: string;
+  private columns: Array<any> = [
+    {
+      title: "姓名",
+      key: "name",
+      render: (h, params) => {
+        return h("div", [
+          h(
+            "span",
+            {
+              style: {
+                display: "inline-block",
+                width: "100%",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                cursor: "pointer"
+              },
+              domProps: {
+                title: params.row.name
+              }
+            },
+            params.row.name
+          )
+        ]);
+      }
+    },
+    {
+      title: "行为",
+      key: "behavior",
+      render: (h, params) => {
+        return h("div", [
+          h(
+            "span",
+            {
+              style: {
+                display: "inline-block",
+                width: "100%",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                cursor: "pointer"
+              },
+              domProps: {
+                title: params.row.behavior
+              }
+            },
+            params.row.behavior
+          )
+        ]);
+      }
+    },
+    {
+      title: "处罚",
+      key: "punish",
+      render: (h, params) => {
+        return h("div", [
+          h(
+            "span",
+            {
+              style: {
+                display: "inline-block",
+                width: "100%",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                cursor: "pointer"
+              },
+              domProps: {
+                title: params.row.punish
+              }
+            },
+            params.row.punish
+          )
+        ]);
+      }
+    },
+    {
+      title: "图片",
+      slot: "img"
+    }
+  ];
   constructor() {
     super();
     this.store = getModule(HomePageStore);
@@ -24,30 +105,35 @@ export default class HomePage extends Vue {
   }
   mounted() {
     this.getJobs();
+    this.getCertificate();
+    this.getCourseware();
+    this.getWorkers();
+    this.getCultivate();
   }
   async getJobs() {
-    let data: Array<jobs>;
     switch (this.jobsType) {
       case "own":
-        data = await this.store.getJobs("own");
-        this.jobs(data);
+        await this.store.getJobs("own").then(res => {
+          this.jobs(res);
+        });
         break;
       case "epiboly":
-        data = await this.store.getJobs("epiboly");
-        this.jobs(data);
+        await this.store.getJobs("epiboly").then(res => {
+          this.jobs(res);
+        });
         break;
       default:
         break;
     }
   }
-  jobs(data: Array<jobs> = [{ value: 0, name: "无数据", color: '#2f5ee4' }]) {
-    let color = data.map(a => a.color)
+  jobs(data: Array<Jobs> = [{ value: 0, name: "无数据", color: "#2f5ee4" }]) {
+    let color = data.map(a => a.color);
     let list = data.map(a => {
       return {
         value: a.value,
         name: a.name
-      }
-    })
+      };
+    });
     const jobs = document.getElementById("jobs");
     const chart: any = this["$echarts"].init(jobs);
     chart.setOption({
@@ -74,11 +160,408 @@ export default class HomePage extends Vue {
       ]
     });
   }
+  async getCertificate() {
+    await this.store.certificate().then(res => {
+      this.certificate(res);
+    });
+  }
+  certificate(
+    data: Array<Certificate> = [
+      { name: "无数据", hasCertificate: 0, inexistence: 0 }
+    ]
+  ) {
+    let xAxisData = data.map(a => a.name);
+    let hasCertificate = data.map(a => a.hasCertificate);
+    let inexistence = data.map(a => a.inexistence);
+    const certificate = document.getElementById("certificate");
+    const chart: any = this["$echarts"].init(certificate);
+    chart.setOption({
+      title: {
+        show: true,
+        text: "人数",
+        left: 15,
+        textStyle: {
+          color: "#747fa0",
+          fontWeight: "normal",
+          fontSize: 12
+        }
+      },
+      legend: {
+        data: ["持证人数", "未持证人数"],
+        right: 20,
+        top: 0
+      },
+      grid: {
+        top: 30,
+        bottom: 30,
+        left: 50,
+        right: 20
+      },
+      xAxis: {
+        data: xAxisData,
+        axisLine: {
+          lineStyle: {
+            color: "#ccc"
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#747fa0"
+          }
+        },
+        splitLine: {
+          show: true
+        }
+      },
+      yAxis: {
+        minInterval: 1,
+        axisLine: {
+          lineStyle: {
+            color: "#ccc"
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#888888"
+          }
+        }
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow"
+        }
+      },
+      series: [
+        {
+          name: "持证人数",
+          type: "bar",
+          stack: "one",
+          barMaxWidth: 40,
+          emphasis: {
+            itemStyle: {
+              barBorderWidth: 1,
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowColor: "rgba(0,0,0,0.5)"
+            }
+          },
+          data: hasCertificate,
+          color: new this["$echarts"].graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: "#1ed7bf"
+            },
+            {
+              offset: 1,
+              color: "#82a5fc"
+            }
+          ])
+        },
+        {
+          name: "未持证人数",
+          type: "bar",
+          stack: "one",
+          data: inexistence,
+          color: "#ccc",
+          barMaxWidth: 40,
+          itemStyle: {
+            normal: {
+              barBorderRadius: [5, 5, 0, 0]
+            }
+          }
+        }
+      ]
+    });
+  }
+  courseware(data: Array<Courseware> = [{ name: "无数据", value: 0 }]) {
+    let yAxisData = data.map(a => a.name);
+    let list = data.map(a => a.value);
+    const courseware = document.getElementById("courseware");
+    const chart: any = this["$echarts"].init(courseware);
+    chart.setOption({
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow"
+        }
+      },
+      grid: {
+        top: 0,
+        bottom: 10,
+        left: 100,
+        right: 20
+      },
+      yAxis: {
+        type: "category",
+        data: yAxisData,
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      xAxis: {
+        show: false
+      },
+      series: [
+        {
+          data: list,
+          type: "bar",
+          barMaxWidth: 40,
+          color: new this["$echarts"].graphic.LinearGradient(0, 0, 1, 1, [
+            {
+              offset: 0,
+              color: "#2f5ee4"
+            },
+            {
+              offset: 1,
+              color: "#7a8ffc"
+            }
+          ]),
+          itemStyle: {
+            normal: {
+              barBorderRadius: [0, 40, 40, 0]
+            }
+          },
+          label: {
+            show: true,
+            position: "right",
+            color: "#7a8ffc"
+          }
+        }
+      ]
+    });
+  }
+  async getCourseware() {
+    await this.store.courseware().then(res => {
+      this.courseware(res);
+    });
+  }
+  workers(data: Array<Workers> = [{ name: "无数据", value: 0 }]) {
+    let xAxisData = data.map(a => a.name);
+    let list = data.map(a => a.value);
+    const workers = document.getElementById("workers");
+    const chart: any = this["$echarts"].init(workers);
+    chart.setOption({
+      title: {
+        show: true,
+        text: "人数",
+        left: 15,
+        textStyle: {
+          color: "#747fa0",
+          fontWeight: "normal",
+          fontSize: 12
+        }
+      },
+      grid: {
+        top: 30,
+        bottom: 30,
+        left: 50,
+        right: 20
+      },
+      xAxis: {
+        data: xAxisData,
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#747fa0"
+          }
+        }
+      },
+      yAxis: {
+        minInterval: 1,
+        axisLine: {
+          show: false,
+          lineStyle: {
+            color: "#ccc"
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#888888"
+          }
+        },
+        splitLine: {
+          show: false
+        }
+      },
+      series: [
+        {
+          type: "bar",
+          barMaxWidth: 20,
+          emphasis: {
+            itemStyle: {
+              barBorderWidth: 1,
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowColor: "rgba(0,0,0,0.5)"
+            }
+          },
+          data: list,
+          color: new this["$echarts"].graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: "#4243e3"
+            },
+            {
+              offset: 1,
+              color: "#96b1fe"
+            }
+          ]),
+          itemStyle: {
+            normal: {
+              barBorderRadius: [20, 20, 20, 20]
+            }
+          },
+          label: {
+            show: true,
+            position: "top"
+          }
+        }
+      ]
+    });
+  }
+  async getWorkers() {
+    await this.store.workers().then(res => {
+      this.workers(res);
+    });
+  }
+  cultivate(data: Array<Cultivate> = [{ name: "无数据", value: 0 }]) {
+    let xAxisData = data.map(a => a.name);
+    let list = data.map(a => a.value);
+    const cultivate = document.getElementById("cultivate");
+    const chart: any = this["$echarts"].init(cultivate);
+    chart.setOption({
+      title: {
+        show: true,
+        text: "人数",
+        left: 15,
+        textStyle: {
+          color: "#747fa0",
+          fontWeight: "normal",
+          fontSize: 12
+        }
+      },
+      grid: {
+        top: 30,
+        bottom: 30,
+        left: 50,
+        right: 20
+      },
+      xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: xAxisData,
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: "#747fa0"
+          }
+        }
+      },
+      yAxis: {
+        type: "value",
+        minInterval: 1,
+        axisLine: {
+          show: false,
+          lineStyle: {
+            color: "#ccc"
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          showMinLabel: false,
+          textStyle: {
+            color: "#888888"
+          }
+        },
+        splitLine: {
+          show: false
+        }
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "cross"
+        }
+      },
+      series: [
+        {
+          data: list,
+          type: "line",
+          color: "#677cfc",
+          areaStyle: {
+            color: new this["$echarts"].graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: "#677cfc"
+              },
+              {
+                offset: 1,
+                color: "#f2f4ff"
+              }
+            ])
+          },
+          smooth: true
+        }
+      ]
+    });
+  }
+  async getCultivate() {
+    await this.store.cultivate().then(res => {
+      this.cultivate(res);
+    });
+  }
+  rowClassName(row, index) {
+    return "table-header";
+  }
 }
-interface jobs {
+interface Jobs {
   value: number;
   name: string;
   color: string;
+}
+interface Certificate {
+  name: string;
+  hasCertificate: number;
+  inexistence: number;
+}
+interface Courseware {
+  name: string;
+  value: number;
+}
+interface Workers {
+  name: string;
+  value: number;
+}
+interface Cultivate {
+  name: string;
+  value: number;
 }
 </script>
 
--- src/store/modules/HomePageStore.ts
+++ src/store/modules/HomePageStore.ts
@@ -1,7 +1,7 @@
 /*
  * @Date         : 2020-04-28 10:01:44
  * @LastEditors  : HaoJie
- * @LastEditTime : 2020-04-28 18:28:38
+ * @LastEditTime : 2020-04-29 19:45:16
  * @FilePath     : \src\store\modules\HomePageStore.ts
  */
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
@@ -18,21 +18,166 @@ import { Message } from "iview";
   store,
 })
 export default class HomePageStore extends VuexModule {
+  public tableData: Array<TableData>;
   constructor(e) {
     super(e);
+    this.tableData = [
+      {
+        name: "啊调查",
+        behavior: "打架",
+        punish: "停职三个月",
+        photoId: 123,
+      },
+      {
+        name: "啊调查",
+        behavior: "打架",
+        punish: "停职三个月",
+        photoId: 123,
+      },
+      {
+        name: "啊调查",
+        behavior: "打架",
+        punish: "停职三个月",
+        photoId: 123,
+      },
+      {
+        name: "啊调查",
+        behavior: "打架",
+        punish: "停职三个月",
+        photoId: 123,
+      },
+    ];
   }
   @Action
   async getJobs(key) {
     switch (key) {
       case "own":
         // 这里请求自有工人数据
-        return [{ name: "自有工人", value: 123, color: "pink" }];
+        return new Promise((res, rej) => {
+          setTimeout(() => {
+            res([{ name: "自有工人", value: 123, color: "pink" }]);
+          }, 1000);
+        });
       case "epiboly":
         // 这里请求分包工人数据
-        return [{ name: "分包工人", value: 123, color: "red" }];
+        return new Promise((res, rej) => {
+          setTimeout(() => {
+            res([{ name: "分包工人", value: 123, color: "red" }]);
+          }, 1000);
+        });
       default:
-        return
+        return;
     }
   }
+  @Action
+  async certificate() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res([
+          {
+            name: "架子工",
+            hasCertificate: 20,
+            inexistence: 100,
+          },
+          {
+            name: "水泥工",
+            hasCertificate: 40,
+            inexistence: 10,
+          },
+          {
+            name: "钢筋工",
+            hasCertificate: 432,
+            inexistence: 324,
+          },
+          {
+            name: "小工",
+            hasCertificate: 433,
+            inexistence: 123,
+          },
+        ]);
+      }, 1000);
+    });
+  }
+  @Action
+  async courseware() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res([
+          {
+            name: "安全类",
+            value: 20,
+          },
+          {
+            name: "取证类",
+            value: 40,
+          },
+          {
+            name: "管理类",
+            value: 432,
+          },
+          {
+            name: "专业技能类",
+            value: 433,
+          },
+        ]);
+      }, 1000);
+    });
+  }
+  @Action
+  async workers() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res([
+          {
+            name: "架子工",
+            value: 20,
+          },
+          {
+            name: "水泥工",
+            value: 40,
+          },
+          {
+            name: "钢筋工",
+            value: 432,
+          },
+          {
+            name: "小工",
+            value: 433,
+          },
+        ]);
+      }, 1000);
+    });
+  }
+  @Action
+  async cultivate() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res([
+          {
+            name: "1月",
+            value: 20,
+          },
+          {
+            name: "2月",
+            value: 40,
+          },
+          {
+            name: "3月",
+            value: 432,
+          },
+          {
+            name: "4月",
+            value: 433,
+          },
+        ]);
+      }, 1000);
+    });
+  }
   // @Mutation
+}
+interface TableData {
+  name: string;
+  behavior: string;
+  punish: string;
+  photoId: number;
 }
