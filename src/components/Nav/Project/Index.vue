75c7037fe5c10cf63129491b7b031d39c9487b31
legendary <legendary_ly@163.com>
2020-04-29 09:53:00 +0800

    已修改        src/components/Nav/Nav.vue 已修改        src/components/Nav/Project/Index.vue 已修改        src/store/modules/ProjectStore.ts 已修改        src/styles/project.css 已修改        src/views/mobile/details.pug 已修改        src/views/nav.pug 已修改        src/views/project.pug 已修改        src/views/worker.pug

--- src/components/Nav/Project/Index.vue
+++ src/components/Nav/Project/Index.vue
@@ -23,6 +23,8 @@
     })
     export default class Project extends Vue {
         private loading = false;
+        public store: any;
+
         messageWarningFn (text) {
             let alert: any = Message;
             alert.warning(text);
@@ -35,156 +37,32 @@
         }
         constructor() {
             super();
-
+            this.store = getModule(ProjectStore);
         }
         mounted() {
+            this.store.getCompany("");
 
         }
-        public buttonProps={
-            type: 'default',
-            size: 'small',
+        getProjectList(){
+            return this.store.companyList;
         }
-
-        public data5:Array<any> = [
-            {
-                title: 'parent 1',
-                expand: true,
-                render: (h, { root, node, data }) => {
-                    return h('span', {
-                        style: {
-                            display: 'inline-block',
-                            width: '100%'
-                        }
-                    }, [
-                        h('span', [
-                            h('Icon', {
-                                props: {
-                                    type: 'ios-folder-outline'
-                                },
-                                style: {
-                                    marginRight: '8px'
-                                }
-                            }),
-                            h('span', data.title)
-                        ]),
-                        h('span', {
-                            style: {
-                                display: 'inline-block',
-                                float: 'right',
-                                marginRight: '32px'
-                            }
-                        }, [
-                            h('Button', {
-                                props: Object.assign({}, this.buttonProps, {
-                                    icon: 'ios-add',
-                                    type: 'primary'
-                                }),
-                                style: {
-                                    width: '64px'
-                                },
-                                on: {
-                                    click: () => { this.append(data) }
-                                }
-                            })
-                        ])
-                    ]);
-                },
-                children: [
+        loadData (item, callback) {
+            setTimeout(() => {
+                const data = [
                     {
-                        title: 'child 1-1',
-                        expand: true,
-                        children: [
-                            {
-                                title: 'leaf 1-1-1',
-                                expand: true
-                            },
-                            {
-                                title: 'leaf 1-1-2',
-                                expand: true
-                            }
-                        ]
+                        title: 'children',
+                        loading: false,
+                        children: []
                     },
                     {
-                        title: 'child 1-2',
-                        expand: true,
-                        children: [
-                            {
-                                title: 'leaf 1-2-1',
-                                expand: true
-                            },
-                            {
-                                title: 'leaf 1-2-1',
-                                expand: true
-                            }
-                        ]
+                        title: 'children',
+                        loading: false,
+                        children: []
                     }
-                ]
-            }
-        ]
-        append(data){
-            const children = data.children || [];
-            children.push({
-                title: 'appended node',
-                expand: true
-            });
-            this.$set(data, 'children', children);
+                ];
+                callback(data);
+            }, 1000);
         }
-
-        remove (root, node, data) {
-            const parentKey = root.find(el => el === node).parent;
-            const parent = root.find(el => el.nodeKey === parentKey).node;
-            const index = parent.children.indexOf(data);
-            parent.children.splice(index, 1);
-        }
-
-        renderContent (h, { root, node, data }) {
-            return h('span', {
-                style: {
-                    display: 'inline-block',
-                    width: '100%'
-                }
-            }, [
-                h('span', [
-                    h('Icon', {
-                        props: {
-                            type: 'ios-paper-outline'
-                        },
-                        style: {
-                            marginRight: '8px'
-                        }
-                    }),
-                    h('span', data.title)
-                ]),
-                h('span', {
-                    style: {
-                        display: 'inline-block',
-                        float: 'right',
-                        marginRight: '32px'
-                    }
-                }, [
-                    h('Button', {
-                        props: Object.assign({}, this.buttonProps, {
-                            icon: 'ios-add'
-                        }),
-                        style: {
-                            marginRight: '8px'
-                        },
-                        on: {
-                            click: () => { this.append(data) }
-                        }
-                    }),
-                    h('Button', {
-                        props: Object.assign({}, this.buttonProps, {
-                            icon: 'ios-remove'
-                        }),
-                        on: {
-                            click: () => { this.remove(root, node, data) }
-                        }
-                    })
-                ])
-            ]);
-        }
-
 
 
 
