3fba7b53bca8629dfb70253a07847715cf9634eb
legendary <legendary_ly@163.com>
2020-05-01 16:13:48 +0800

    已修改        src/components/Nav/Lecturer/Index.vue 已修改        src/components/Nav/Nav.vue 已修改        src/store/modules/LecturerStore.ts 已修改        src/styles/lecturer.css 已修改        src/styles/worker.css 已修改        src/views/lecturer.pug 已修改        src/views/nav.pug

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -8,18 +8,6 @@
     import WorkerStore from '../../store/modules/WorkerStore';
 
     @Component({
-        data() {
-            return {
-                loading: false
-            }
-        },
-        components:{
-        },
-        directives: { // 自定义指令
-        },
-        mounted() {
-            this.select(this['$router'].currentRoute.path, true)
-        },
         computed: {
             menuitemClasses: () => {
                 return [
@@ -36,41 +24,17 @@
         private store: any;
         private workerStore: any;
 
-        private homepage: boolean;              //主页
-        private projectManagement = true;     //项目管理
-        private peopleManagement: boolean;     //人员管理
-        private trainingManagement: boolean;   //培训管理
-        private lecturerManagement: boolean;   //讲师管理
-        private comprehensiveAssessment: boolean;   //综合评价
-        private badEvaluation: boolean;           //不良评价
-        private skillAppraisal: boolean;           //技能鉴定
-        private setting: boolean;           //设置
 
         private type: string;           //设置
         private style: string;
         private menuName: string;
         private path: string;
 
-
-
-        private styleClass: string;
-
         constructor() {
             super();
             this.store = getModule(NavStore)
             this.workerStore = getModule(WorkerStore)
 
-            this.homepage = false;              //主页
-            this.projectManagement = false;     //项目管理
-            this.peopleManagement  = true;     //人员管理
-            this.trainingManagement  = false;   //培训管理
-            this.lecturerManagement  = false;   //讲师管理
-            this.comprehensiveAssessment  = false;   //综合评价
-            this.badEvaluation  = false;           //不良评价
-            this.skillAppraisal  = false;           //技能鉴定
-            this.setting  = false;           //设置
-            this.type=null;
-            this.styleClass = null;
             let path = {'homepage':'','projectManagement':'','peopleManagement':'',
                 'trainingManagement':'','lecturerManagement':'','comprehensiveAssessment':'',
                 'badEvaluation':'','skillAppraisal':'','setting':''};
@@ -87,16 +51,13 @@
 
         }
         clickMenu(checked,menuName,path){
+            debugger
             //一级菜单
             let menu = ["homepage","projectManagement","peopleManagement","trainingManagement","lecturerManagement",
                 "comprehensiveAssessment","badEvaluation","skillAppraisal","setting"];
             for(let i = 0;i < menu.length; i++){
                 if(menu[i]==checked){
-                    this[checked] = true;
                     this.store.setMenuList(menuName);
-                    this['$router'].push(path);
-                }else {
-                    this[`${menu[i]}`] = false;
                     this.type = checked;
                     this.menuName = menuName;
                     JSON.stringify(sessionStorage.setItem("type",checked));
@@ -112,34 +73,27 @@
         }
 
         switchChecked(switchName) {
-            this.styleClass = switchName;
             //二级菜单
             this.style = switchName;
             JSON.stringify(sessionStorage.setItem("style",switchName));
-                if (switchName == 'theirOwn') {
-                    this.workerStore.setSelectEafUserStatus(1);
-                    this.workerStore.searchUserList();
-                } else if (switchName == 'exterior') {
-                    this.workerStore.setSelectEafUserStatus(2);
-                    this.workerStore.searchUserList();
-                }else{
-                    //二级菜单路由跳转
-                    JSON.stringify(sessionStorage.setItem("path",switchName));
+            if (switchName == 'theirOwn') {
+                this.workerStore.setSelectEafUserStatus(1);
+                this.workerStore.searchUserList();
+            } else if (switchName == 'exterior') {
+                this.workerStore.setSelectEafUserStatus(2);
+                this.workerStore.searchUserList();
+            }else if(switchName == 'externalLecturer'){
 
-                    this['$router'].push(switchName);
-                }
+            }else if(switchName == 'internalTrainer'){
 
-        }
+            }
+            else{
+                //二级菜单路由跳转
+                JSON.stringify(sessionStorage.setItem("path",switchName));
 
-        switchMode(switchName){
-            // let switchMenu = ["account",s"workclass","commentType","grade","rate","course","punishment","indexVideo"];
-            // for(let i = 0;i < switchMenu.length; i++){
-            //     if(switchMenu[i]==switchName){
-            //
-            //         this.style = switchName;
-            //
-            //     }
-            // }
+                this['$router'].push(switchName);
+            }
+
         }
 
 
